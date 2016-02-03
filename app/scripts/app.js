63f7a863ab30a80c51b3a8a5878591ae22a9dc42
Hamdi <hamdiceylan@gmail.com>
2016-02-03 21:51:59 +0000

    First Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+var flickrApp = angular.module('flickrApp',[]);
+
+flickrApp.controller("MainCtrl", function ($scope, $http) {
+    $scope.getData = function (searchField) {
+      $scope.loading = true;
+      $http.jsonp("http://www.flickr.com/services/feeds/photos_public.gne?tags="+searchField+"&format=json&jsoncallback=JSON_CALLBACK").
+        success(function (data) {
+          $scope.images = data.items;
+          $scope.loading = false;
+        }).
+        error(function (data) {
+          $scope.images = "Request failed";
+          $scope.loading = false;
+        });
+    }
+});
